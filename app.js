9a03da97cde8d33dc465916d2b1e95943fdcd06c
James Sieu <jamessieu@gmail.com>
2018-12-25 00:41:47 -0800

    added alert when book is successfully added to list

--- app.js
+++ app.js
@@ -101,6 +101,9 @@ document.querySelector('#book-form').addEventListener('submit', e => {
     // Add Book to UI
     UI.addBookToList(book);
 
+    // Show success message
+    UI.showAlert('Book Added', 'success');
+
     // Clear fields
     UI.clearFields();
   }
@@ -109,4 +112,7 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 //Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', e => {
   UI.deleteBook(e.target);
+
+  // Show success message
+  UI.showAlert('Book removed', 'success');
 });
