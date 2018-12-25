ab702f74183223286ffb351d04e7294978ac6287
James Sieu <jamessieu@gmail.com>
2018-12-25 00:39:34 -0800

    added alert when form is not filled in on submission

--- app.js
+++ app.js
@@ -56,6 +56,19 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    // Vanish in 3 seconds
+    setTimeout(() => {
+      document.querySelector('.alert').remove();
+    }, 3000);
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -78,14 +91,19 @@ document.querySelector('#book-form').addEventListener('submit', e => {
   const author = document.querySelector('#author').value;
   const isbn = document.querySelector('#isbn').value;
 
-  // Instantiate book
-  const book = new Book(title, author, isbn);
+  // Validate
+  if (title === '' || author === '' || isbn === '') {
+    UI.showAlert(' Please fill in all fields', 'danger');
+  } else {
+    // Instantiate book
+    const book = new Book(title, author, isbn);
 
-  // Add Book to UI
-  UI.addBookToList(book);
+    // Add Book to UI
+    UI.addBookToList(book);
 
-  // Clear fields
-  UI.clearFields();
+    // Clear fields
+    UI.clearFields();
+  }
 });
 
 //Event: Remove a Book
