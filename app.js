4bc7eb53452fdd468a0e2454fc8ed479a0f19322
James Sieu <jamessieu@gmail.com>
2018-12-25 00:28:26 -0800

    added delete functionality to list

--- app.js
+++ app.js
@@ -50,6 +50,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -83,3 +89,6 @@ document.querySelector('#book-form').addEventListener('submit', e => {
 });
 
 //Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', e => {
+  UI.deleteBook(e.target);
+});
